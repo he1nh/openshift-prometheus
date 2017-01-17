2250700bcb73473f4fa735bc41ec9d37c0e92080
Hein Hermans <hein.hermans@gmail.com>
2017-01-17 10:59:34 +0100

    more verbose output for demo

--- create_serviceaccounts.sh
+++ create_serviceaccounts.sh
@@ -3,8 +3,11 @@
 oc create -f objects/serviceaccounts.yml
 
 # in images from Docker Hub both prometheus and grafana expect to run as root user
+echo "adding security context constrain 'anyuid' to user prometheus"
 oc adm policy add-scc-to-user anyuid -z prometheus
+echo "adding security context constrain 'anyuid' to user grafana"
 oc adm policy add-scc-to-user anyuid -z grafana
 
 # add cluster-reader role to prometheus in default namespace. For prometheus service discovery (SD) to work
+echo "adding cluster-role 'cluster-reader' to user prometheus in namespace 'default'"
 oc adm policy add-cluster-role-to-user cluster-reader system:serviceaccount:default:prometheus
